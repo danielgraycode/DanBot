7eb767027ff3cc31f118234391cfecc9a644c66d
TheCoderGuy1 <accounts@danielgray.me>
2017-03-14 18:37:59 +0000

    Do I have to add the files everytime?

---
+++ main.js
@@ -0,0 +1,31 @@
+/*Copyright (c) 2017 Daniel Gray
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+
+const discord = require("discord.js");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require("assert");
+const url = 'mongodb://danielserver.local:27017/DanBot';
+const client = new discord.Client();
+const config = require("./config.json")
+
+MongoClient.connect(url, function(error, db) {
+  assert.equal(null, err);
+  console.log("I'm connected to the database!")
+});
